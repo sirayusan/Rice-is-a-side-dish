9b8c8a9751358ebd80934ea3db640cfdb1b23446
sirayu <sirayusan3636@gmail.com>
2020-10-29 08:50:48 +0900

    こめんと

--- laravel/config/fortify.php
+++ laravel/config/fortify.php
@@ -102,15 +102,15 @@ return [
     |
     */
 
-    'features' => [
-        Features::registration(),
-        Features::resetPasswords(),
-        // Features::emailVerification(),
-        Features::updateProfileInformation(),
-        Features::updatePasswords(),
-        Features::twoFactorAuthentication([
-            'confirmPassword' => true,
-        ]),
-    ],
+    // 'features' => [
+    //     Features::registration(),
+    //     Features::resetPasswords(),
+    //     // Features::emailVerification(),
+    //     Features::updateProfileInformation(),
+    //     Features::updatePasswords(),
+    //     Features::twoFactorAuthentication([
+    //         'confirmPassword' => true,
+    //     ]),
+    // ],
 
 ];
