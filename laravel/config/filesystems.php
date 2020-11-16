2526e2d9a4b7dd67ff2fe3de93a0b4e7dfe013bf
sirayu <sirayusan3636@gmail.com>
2020-11-16 21:15:39 +0900

    不要ファイル削除

--- laravel/config/filesystems.php
+++ laravel/config/filesystems.php
@@ -46,6 +46,14 @@ return [
         'local' => [
             'driver' => 'local',
             'root' => storage_path('app'),
+            'permissions' => [
+                'file' => [
+                    'public' => 0664,
+                ],
+                'dir' => [
+                    'public' => 0775,
+                ],
+            ],
         ],
 
         'public' => [
