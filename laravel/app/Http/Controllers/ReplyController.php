7ce08dedee8422fa423c0232467811050d507128
sirayu <sirayusan3636@gmail.com>
2020-12-08 16:26:02 +0900

    不要部削除

--- laravel/app/Http/Controllers/ReplyController.php
+++ laravel/app/Http/Controllers/ReplyController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Reply;
 use App\Models\Post;
-use App\Models\Favorite;
 use Auth;
 
 class ReplyController extends Controller
--- laravel/app/Models/Post.php
+++ laravel/app/Models/Post.php
@@ -6,7 +6,6 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Favorite;
 use Auth;
-use DB;
 
 class Post extends Model
 {
