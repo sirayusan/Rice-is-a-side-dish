9721b0c5463e82c133161c91135883754bfbaf65
sirayu <sirayusan3636@gmail.com>
2021-01-06 20:52:42 +0900

    タグ検索機能

--- laravel/app/Http/Controllers/TagController.php
+++ laravel/app/Http/Controllers/TagController.php
@@ -8,92 +8,23 @@ use App\Models\Tag;
 class TagController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     * タグ検索欄の値を展開しタグインスタンスを取得して更に展開して投稿を取得する。
-     */
+    * Display a listing of the resource.
+    *
+    * @return \Illuminate\Http\Response
+    * タグ検索欄の値を展開しタグインスタンスを取得して更に展開して投稿を取得する。
+    */
     public function index(Request $request)
     {
         $substitute_tags = explode(',',str_replace('，',',',$request->tags));
-        foreach ($substitute_tags as $substitute_tag)
+        $tags = Tag::wherein('tag',$substitute_tags)->get();
+        foreach ($tags as $tag)
         {
-            $tags = Tag::where('tag',$substitute_tag)->get();
-            foreach ($tags as $tag)
+            foreach ($tag->posts as $post)
             {
-                foreach ($tag->posts as $post)
-                {
-                    $posts[] = $post;
-                }
+                $posts[] = $post;
             }
         }
         $posts = array_unique($posts);
         return view('tag_search',compact('posts'));
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 }
